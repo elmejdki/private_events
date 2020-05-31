818943b9392d6ce0be8f5046cb38a3c39f10b7e8
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2020-05-31 00:04:54 +0000

    Fix many to many association bettween user and event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
-  has_many :events_users, foreign_key: :attendee_id
   belongs_to :creator, class_name: 'User', foreign_key: :user_id
+
+  has_many :events_users, foreign_key: :attended_event_id
   has_many :attendees, through: :events_users, source: :attendee
-  #has_and_belongs_to_many :attendees, class_name: 'User', foreign_key: :user_id
 end
