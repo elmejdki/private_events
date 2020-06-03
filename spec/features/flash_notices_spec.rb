bef5438cade547a8452e770ffd85eb2fb46ca001
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2020-06-03 22:08:36 +0100

    Add session tests

--- spec/features/flash_notices_spec.rb
+++ spec/features/flash_notices_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe 'Flash Notices for sign in', js:true do
   let!(:user) { User.create(username: username, email: email) }
+  let!(:second_user) { User.create(username: username + 'fs', email: email + 'fdsf') }
   let(:email) { 'Zack@gmail.com' }
   let(:username) { 'Zack' }
   let(:wrong_username) { 'nothing' }
@@ -10,30 +11,43 @@ describe 'Flash Notices for sign in', js:true do
     visit users_sign_in_path
   end
 
-  it 'user can\'t log in' do
-    fill_in 'Username', with: wrong_username
-    click_button 'Log in'
-
-    expect(page).to have_content('Wrong Username, sorry you are not allowed to enter.')
-  end
-
-  it 'user logged in succefully' do
-    fill_in 'Username', with: username
-    click_button 'Log in'
-
-    expect(page).to have_content('You signed in successfully.')
-  end
-
-  it 'sign up a user successfuly' do
-    visit new_user_path
-    fill_in 'Username', with: 'newuser'
-    fill_in 'Email', with: 'newuser@gmail.com'
-    click_button 'Sign up'
-
-    expect(page).to have_content('You have successfuly signed up.')
+  context 'Unsigned in user' do
+    it 'user can\'t log in' do
+      fill_in 'Username', with: wrong_username
+      click_button 'Log in'
+  
+      expect(page).to have_content('Wrong Username, sorry you are not allowed to enter.')
+    end
+  
+    it 'user logged in succefully' do
+      fill_in 'Username', with: username
+      click_button 'Log in'
+  
+      expect(page).to have_content('You signed in successfully.')
+    end
+  
+    it 'sign up a user successfuly' do
+      visit new_user_path
+      fill_in 'Username', with: 'newuser'
+      fill_in 'Email', with: 'newuser@gmail.com'
+      click_button 'Sign up'
+  
+      expect(page).to have_content('You have successfuly signed up.')
+    end
   end
   
-  context 'User is signed in' do
+  context 'Signed in user' do
+    let!(:event) { User.first.events.create(
+      title: 'Event Title',
+      body: body,
+      place: place,
+      date: date
+    ) }
+    let(:title) { 'Test Event' }
+    let(:body) { 'Test Event for testing purposes' }
+    let(:place) { 'Far away' }
+    let(:date) { Time.now }
+
     before do
       fill_in 'Username', with: username
       click_button 'Log in'
@@ -51,12 +65,27 @@ describe 'Flash Notices for sign in', js:true do
         click_link 'My Events'
       end
       click_link 'Create a new event'
-      fill_in 'Title', with: 'Test Event'
-      fill_in 'Body', with: 'Test Event for testing purposes'
-      fill_in 'Place', with: 'Far away'
-      fill_in 'Date', with: Time.now
+
+      fill_in 'Title', with: title
+      fill_in 'Body', with: body
+      fill_in 'Place', with: place
+      fill_in 'Date', with: date
+
       click_button 'Create Event'
       expect(page).to have_content('A new event was created successfuly')
     end
+
+    it 'create event successfully' do
+      within('.navbar') do
+        click_link 'All Events'
+      end
+
+      within('.box') do
+        click_link 'Show'
+      end
+
+      click_button 'invite user'
+      expect(page).to have_content('user was invited to the event successfully')
+    end    
   end
 end
---
+++ spec/features/sessions_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+describe 'User Sessions' do
+  let!(:user) { User.create(username: username, email: email) }
+  let(:username) { 'Zack' }
+  let(:email) { 'Zack@Efrone.net' }
+
+  before do
+    visit root_path
+    within('.navbar') { click_link('Sign in') }
+  end
+
+  context 'user unsigned in' do
+    before do
+      fill_in 'Username', with: ''
+      click_button 'Log in'
+    end
+
+    it 'displays an error message' do
+      expect(page).to have_content('Wrong Username, sorry you are not allowed to enter.')
+    end
+
+    it 'shows the correct menu links' do
+      within('.navbar') do
+        expect(page).to have_link('Sign in')
+        expect(page).to have_link('Sign up')
+        expect(page).to_not have_link('My Events')
+        expect(page).to_not have_link('Users')
+        expect(page).to_not have_link('All Events')
+        expect(page).to_not have_link('Sign Out')
+      end
+    end
+  end
+
+  context 'user unsigned in' do
+    before do
+      fill_in 'Username', with: username
+      click_button 'Log in'
+    end
+
+    it 'displays a welcome message' do
+      expect(page).to have_content('You signed in successfully.')
+    end
+
+    it 'shows the correct menu links' do
+      within('.navbar') do
+        expect(page).to have_link('My Events')
+        expect(page).to have_link('Users')
+        expect(page).to have_link('All Events')
+        expect(page).to have_link('Sign Out')
+        expect(page).to_not have_link('Sign in')
+        expect(page).to_not have_link('Sign up')
+      end
+    end
+  end
+end
