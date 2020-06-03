239a603dc41652e4a39c8a0c3ecc32fe5996fb00
FrancoRosa <km115.franco@gmail.com>
2020-06-03 15:19:31 -0500

    Add test for new event

--- spec/features/flash_notices_spec.rb
+++ spec/features/flash_notices_spec.rb
@@ -32,4 +32,31 @@ describe 'Flash Notices for sign in', js:true do
 
     expect(page).to have_content('You have successfuly signed up.')
   end
+  
+  context 'User is signed in' do
+    before do
+      fill_in 'Username', with: username
+      click_button 'Log in'
+    end
+    
+    it 'sign out successfuly' do
+      within('.navbar') do
+        click_link 'Sign Out'
+      end
+      expect(page).to have_content('You signed out, see you later man')
+    end
+    
+    it 'create event successfully' do
+      within('.navbar') do
+        click_link 'My Events'
+      end
+      click_link 'Create a new event'
+      fill_in 'Title', with: 'Test Event'
+      fill_in 'Body', with: 'Test Event for testing purposes'
+      fill_in 'Place', with: 'Far away'
+      fill_in 'Date', with: Time.now
+      click_button 'Create Event'
+      expect(page).to have_content('A new event was created successfuly')
+    end
+  end
 end
