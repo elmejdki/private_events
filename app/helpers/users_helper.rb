3550a7974f14c0a3d38a79eec6b7715542277a56
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2020-06-05 22:36:31 +0100

    Refactor trash icon

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,10 @@ module UsersHelper
       render partial: 'error_handler', locals: {source: field}
     end
   end
+
+  def trash_action
+    if @user.id == session[:user_id]
+      render 'trash_icon'
+    end
+  end
 end
